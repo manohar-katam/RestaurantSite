806080adf003e27e83001cec7f8d738d33f8562e
kmreddyhyd <kmreddyhyd@gmail.com>
2017-02-08 18:38:46 -0600

    This is a dynamic restaurant site
    • Firstly, developed a static restaurent site for Aritaku Bhojanam
    restaurent using HTML, CSS, Bootstrap.
    • In the next step, manipulated the web page components using Javascript
    Document Object Model API.
    • Finally, connected our restaurant web site to a real backend service
    using JSON & Ajax which pulls the data for the restaurant
    menu dynamically from the server without having to reload the entire
    page.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* For closing the event section which loads home icons */
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
